983e8ef95cb24c347338b7870e0f7d4de29584d4
vj28 <vijaymourya28@gmail.com>
2020-10-16 22:09:56 +0530

    added json parser (missed in first commit)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+function RenderArray({ keyName, value, tabClass }) {
+  const [isExpanded, setExpand] = useState(false);
+  return (
+    <div className={tabClass}>
+      <div onClick={() => setExpand(!isExpanded)}>
+        {keyName}: array({value.length})
+      </div>
+      {isExpanded &&
+        value.map((x, index) => (
+          <div key={x} className={isExpanded ? "block tab1" : "tab1"}>
+            {index}: {x}
+          </div>
+        ))}
+    </div>
+  );
+}
+
+function renderObject(obj, arr, tabSpaces) {
+  Object.keys(obj).map((x) => {
+    if (Array.isArray(obj[x])) {
+      arr.push(
+        <RenderArray
+          key={x}
+          keyName={x}
+          value={obj[x]}
+          tabClass={"tab" + tabSpaces}
+        />
+      );
+    } else if (typeof obj[x] == "object") {
+      arr.push(<div>{x}: object</div>);
+      renderObject(obj[x], arr, tabSpaces + 1);
+    } else
+      arr.push(
+        <div key={obj[x]} className={"tab" + tabSpaces}>
+          {x}: {obj[x]}
+        </div>
+      );
+  });
+}
 
 function App() {
+  const inputRef = useRef();
+  const [error, setError] = useState("");
+  const [output, setOutput] = useState("");
+  const [treeView, setTreeState] = useState({
+    showTreeView: false,
+    treeData: null,
+  });
+  const handleClick = () => {
+    try {
+      let json = inputRef.current.value;
+      console.log(json);
+      let formattedJSON = JSON.parse(json);
+      setOutput(JSON.stringify(formattedJSON, null, 4));
+      setTreeState({ showTreeView: false });
+    } catch (ex) {
+      setError(ex.message);
+    }
+  };
+  const showTree = () => {
+    try {
+      let json = inputRef.current.value;
+      let formattedJSON = JSON.parse(json);
+      let arr = [];
+      renderObject(formattedJSON, arr, 0);
+      setTreeState({ showTreeView: true, treeData: arr });
+    } catch (ex) {
+      setError(ex.message);
+    }
+  };
+  const copyJson = () => {
+    var copyText = document.getElementById("formattedJson").textContent;
+    const textArea = document.createElement("textarea");
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    alert("Copied the text: " + textArea.textContent);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex">
+        <textarea
+          className="inputBox"
+          ref={inputRef}
+          cols={20}
+          rows={10}
+        ></textarea>
+        <div>
+          <div className="btn" onClick={handleClick}>
+            Parse
+          </div>
+          <div className="btn" onClick={showTree}>
+            Tree Viewer
+          </div>
+          {output && <button onClick={copyJson}>Copy JSON</button>}
+        </div>
+        {treeView.showTreeView ? (
+          <div className="output">{treeView.treeData}</div>
+        ) : (
+          <pre id="formattedJson" className="output">
+            {output ? output : null}
+          </pre>
+        )}
+      </div>
+      {error && <div>{error}</div>}
     </div>
   );
 }
